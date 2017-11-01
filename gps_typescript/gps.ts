e8318dace119788a25bb6b99cec925a0adf4bc41
Jonathon Belotti <jbelotti@zendesk.com>
2017-11-01 20:04:29 +1100

    setup basic typescript project with json import ability

---
+++ gps_typescript/gps.ts
@@ -0,0 +1,5 @@
+export function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = "Jane Doe";
---
+++ gps_typescript/index.ts
@@ -0,0 +1,7 @@
+import { greeter } from './gps';
+import * as example from './examples/monkeys.json';
+
+
+let user = "Jane Doe";
+
+console.log(greeter(user));
