8bdc463659cbef5aa7cca412ebc67268f293c7be
Jonathon Belotti <jbelotti@zendesk.com>
2017-11-02 08:43:19 +1100

    create new Operator class and add start of applyOperator method

--- gps_typescript/gps.ts
+++ gps_typescript/gps.ts
@@ -3,3 +3,32 @@ export function greeter(person: string) {
 }
 
 let user = "Jane Doe";
+
+
+function achieveAll(states: string[], ops: Object[], goals: string[], goalStack: string[]) {
+    return [];
+}
+
+export class Operator {
+    add: string[];
+    delete: string[];
+    preconds: string[];
+
+    constructor(add: string[],
+                del: string[],
+                preconds: string[]) {
+        this.add = add;
+        this.delete = del;
+        this.preconds = preconds;
+    }
+}
+
+export function applyOperator(operator: Operator,
+                       states: string[],
+                       operators: Operator[],
+                       goal: string,
+                       goalStack: string[]) {
+
+    var result = achieveAll(states, operators, operator.preconds, [goal].concat(goalStack));
+    if (!result.length) return [];
+}
--- gps_typescript/index.ts
+++ gps_typescript/index.ts
@@ -1,7 +1,13 @@
-import { greeter } from './gps';
+import { greeter, applyOperator, Operator } from './gps';
 import * as example from './examples/monkeys.json';
 
 
 let user = "Jane Doe";
 
 console.log(greeter(user));
+
+let operator =  new Operator(['yes'], ['no'], ['ready']);
+
+console.log(
+  applyOperator(operator, ['ready', 'banana'], [operator], 'yes', [])
+);
