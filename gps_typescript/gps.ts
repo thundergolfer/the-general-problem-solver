05ac9231f099ca401eb3ad2d6d3019f0f16d9663
Jonathon Belotti <jbelotti@zendesk.com>
2017-11-02 19:32:56 +1100

    complete implementation

--- gps_typescript/gps.ts
+++ gps_typescript/gps.ts
@@ -5,22 +5,77 @@ export function greeter(person: string) {
 let user = "Jane Doe";
 
 
-function achieveAll(states: string[], ops: Object[], goals: string[], goalStack: string[]) {
-    return [];
+export function gps(initialStates: string[], goalStates: string[], operators: Operator[]) {
+    var prefix = 'Executing ';
+    operators.forEach(function(operator: Operator) {
+        operator.add.push(prefix + operator.action);
+    });
+
+    var finalStates = achieveAll(initialStates, operators, goalStates, []);
+
+    if (!finalStates.length) return false;
+
+    var actions = finalStates.filter(function(elem: string) {
+        return elem.startsWith(prefix);
+    });
+
+    return actions;
+}
+
+
+function achieveAll(states: string[], ops: Operator[], goals: string[], goalStack: string[]) {
+    goals.forEach(function (goal: string) {
+        states = achieve(states, ops, goal, goalStack);
+        if (!states.length) return <string[]>[];
+    });
+
+    goals.forEach(function(goal: string) {
+      if (states && states.indexOf(goal) < 0) return [];
+    });
+
+    return states;
 }
 
 export class Operator {
     add: string[];
     delete: string[];
     preconds: string[];
+    action: string;
 
     constructor(add: string[],
                 del: string[],
-                preconds: string[]) {
+                preconds: string[],
+                action: string) {
         this.add = add;
         this.delete = del;
         this.preconds = preconds;
+        this.action = action;
     }
+}
+
+function achieve(states: string[], operators: Operator[], goal: string, goalStack: string[]) {
+  // Let's check to see if the state already holds before we do anything.
+  if (states && states.indexOf(goal) >= 0) {
+      return states;
+  }
+
+  if (goalStack.indexOf(goal) >= 0) { // is this a bug? What if the index of goal is 0 AND 4?
+      return <string[]>[];
+  }
+
+  var result = <string[]>[];
+
+  for(var op of operators) {
+    if (op.add.indexOf(goal) < 0) {
+      continue
+    }
+
+    var result: string[] = applyOperator(op, states, operators, goal, goalStack);
+
+    if (!!result.length) return result;
+  }
+
+  return <string[]>[];
 }
 
 export function applyOperator(operator: Operator,
@@ -31,4 +86,13 @@ export function applyOperator(operator: Operator,
 
     var result = achieveAll(states, operators, operator.preconds, [goal].concat(goalStack));
     if (!result.length) return [];
+
+    let addList = operator.add;
+    let delList = operator.delete;
+
+    var newState = result.filter(function(elem: string) {
+      return this.indexOf(elem) < 0;
+    }, delList);
+
+    return newState.concat(addList);
 }
--- gps_typescript/index.ts
+++ gps_typescript/index.ts
@@ -1,13 +1,15 @@
-import { greeter, applyOperator, Operator } from './gps';
-import * as example from './examples/monkeys.json';
+import { gps, Operator } from './gps';
+import * as example from '../examples/monkeys.json';
 
+// let operator =  new Operator(['yes'], ['no'], ['ready']);
 
-let user = "Jane Doe";
+var operators: Operator[] = [];
+for (var op of (<any>example).ops) {
+  let op_ = new Operator(op.add, op.delete, op.preconds, op.action);
+  operators.push(op_)
+}
 
-console.log(greeter(user));
+let result = gps((<any>example).start, (<any>example).finish, operators);
 
-let operator =  new Operator(['yes'], ['no'], ['ready']);
-
-console.log(
-  applyOperator(operator, ['ready', 'banana'], [operator], 'yes', [])
-);
+console.log('Result: ');
+console.log(result);
